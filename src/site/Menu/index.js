3a50561bfb5fe2511ac656e35391e7e02dd961a2
sam bossley <35381535+bossley9@users.noreply.github.com>
2019-08-22 23:27:43 -0400

    menu active indicator

--- src/site/Menu/index.js
+++ src/site/Menu/index.js
@@ -1,35 +1,30 @@
 import React from 'react';
 import './Menu.scss';
-import { Link as RouterLink, withRouter } from 'react-router-dom'; 
+import { NavLink as RouterLink } from 'react-router-dom';
 import { Menu as UtilityMenu } from 'react-utility-components';
 
 const { Link, SubMenu } = UtilityMenu;
 
+const checkActive = function (href) {
+  return (match, location) => 
+    location ? (location.pathname === href) : false;
+}
+
 class Menu extends React.Component {
-  constructor(props) {
-    super(props);
-    this.getNavLinkClass = this.getNavLinkClass.bind(this);
-  }  
-  getNavLinkClass(path) {
-    return this.props.location.pathname === path ? 'active' : '';
-  }
-  
   render() {
     return (
       <header className='menu'>
         <UtilityMenu
-          // active
           dropdown
-          // horizontal
-          // TODO
-          linkWrapper={(href, children) => <RouterLink to={href}>{children}</RouterLink>}
+          horizontal
+          linkWrapper={function (href, children) { return <RouterLink to={href} isActive={checkActive(href)}>{children}</RouterLink>}}
         >
 
           <Link href='/'>Home</Link>
 
           <SubMenu>
             <Link>About Us</Link>
-            <Link href='/mission'>Our Mission</Link>
+            <Link href='/mission'>Our Mission</Link> 
             <Link href='/board'>Executive Board</Link>
             <Link href='/members'>Member Schools</Link>
             <Link href='/history'>Our History</Link>
@@ -38,9 +33,7 @@ class Menu extends React.Component {
           <SubMenu>
             <Link>Conferences</Link>
             <Link href='/upcoming'>Upcoming</Link>
-            <Link 
-              className={this.getNavLinkClass('/host')}
-              href='/host'>Host a Conference</Link>
+            <Link href='/host'>Host a Conference</Link>
           </SubMenu>
 
           <SubMenu>
@@ -73,4 +66,4 @@ class Menu extends React.Component {
   }
 }
 
-export default withRouter(Menu);
+export default Menu;
