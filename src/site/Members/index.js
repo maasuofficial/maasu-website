49e6c63f805e4b125184a7f3461225246873c50c
sam bossley <35381535+bossley9@users.noreply.github.com>
2019-08-29 00:07:12 -0400

    fixed member school listings

--- src/site/Members/index.js
+++ src/site/Members/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Page } from '../../components';
+import { Page, Type } from '../../components';
 
 class Members extends React.Component {
   componentDidMount() {
@@ -10,53 +10,69 @@ class Members extends React.Component {
     return (
       <React.Fragment>
         <Page>
-        Membership
-The Midwest Asian American Students Union provides resources and support to the Asian Pacific Islander American community and youth. The opportunities for networking, leadership, and personal development through our involvement is invaluable.
-Benefits
-Leadership Summit and Spring Conference registration rates will be discounted
-Gain support from MAASU’s experienced network of leaders in the APIA community about activism and student organization growth.
-Network with students and alumni from the 30+ membership schools of MAASU, as well as various professionals and figures from the APIA community.
-ETC
-For more information on eligibility and benefits, click here or email memoutreach1@gmail.com or memoutreach2@gmail.com.
+          <Type variant='sub2'>Membership</Type>
+          <p>
+            The Midwest Asian American Students Union provides resources and support to the Asian Pacific Islander 
+            American community and youth. The opportunities for networking, leadership, and personal development 
+            through our involvement is invaluable.
+          </p>
+          <Type variant='sub2'>Benefits</Type>
+          <ul>
+            <li>
+              Leadership Summit and Spring Conference registration rates will be discounted
+            </li>
+            <li>
+              Gain support from MAASU’s experienced network of leaders in the APIA community about activism and student organization growth.
+            </li>
+            <li>
+              Network with students and alumni from the 30+ membership schools of MAASU, as well as various professionals and figures from the APIA community.
+            </li>
+          </ul>
+          <p>
+            For more information on eligibility and benefits, contact <a href='mailto:memoutreach@maasu.org'>memoutreach@maasu.org</a>.
+          </p>
+          <Type variant='sub2'>Member Schools</Type>
           {/* last updated 01/14/19 */}
           {/* TODO add links */}
-Augsburg College
-Ball State University
-Case Western Reserve University
-Cardinal Stritch University
-Central Michigan University
-Denison University
-Indiana University Purdue University Indianapolis’s (IUPUI)
-Kalamazoo College
-Kansas State University
-Michigan State University
-Northwestern University
-Northeast Wisconsin Technical College
-Notre Dame University
-Ohio Northern University
-Oklahoma State University
-Purdue University at West Lafayette
-St. Catherine’s University
-St. Louis University
-The Ohio State University
-University of Colorado Boulder
-University of Illinois at Chicago
-University of Illinois at Urbana Champaign
-University of Kansas
-University of Michigan – Ann Arbor
-University of Minnesota – Duluth
-University of Minnesota Twin Cities
-University of Missouri
-University of Missouri
-University of Nebraska Lincoln
-University of Wisconsin – Platteville
-University of Wisconsin – Madison
-Vanderbilt University
-Valparaiso University
-Washington University – St Louis
-Wichita State University
+          <ul>
+            <li><a href='https://www.augsburg.edu/panasian/augsburg-asian-student-association/'>Augsburg University</a></li>
+            <li><a href='https://bsu.edu/aasa'>Ball State University</a></li>
+            <li><a href='https://community.case.edu/aaa/home/'>Case Western Reserve University</a></li>
+            <li><a href='https://www.stritch.edu/Get-Involved/Clubs-Organizations'>Cardinal Stritch University</a></li>
+            <li><a href='https://cmich.campuslabs.com/engage/organization/asian-cultural-organization'>Central Michigan University</a></li>
+            <li><a href='https://denison.edu/campus/diversity/student-organizations'>Denison University</a></li>
+            <li><a href='http://www.iupui.edu/~asunited/'>Indiana University-Purdue University Indianapolis (IUPUI)</a></li>
+            <li><a href='https://kzoo.presence.io/organization/asian-pacific-islander-student-association'>Kalamazoo College</a></li>
+            <li><a href='https://www.k-state.edu/aasu/AsianAmericaStudentUnion/Welcome.html'>Kansas State University</a></li>
+            <li><a href='https://msu.edu/~apaso/about.html'>Michigan State University</a></li>
+            <li><a href='https://www.asianamerican.northwestern.edu/'>Northwestern University</a></li>
+            <li><a href='https://www.nwtc.edu/student-experience/student-involvement/clubs-and-organizations/multicultural/asian-american-student-association'>Northeast Wisconsin Technical College</a></li>
+            <li><a href='https://www3.nd.edu/~aaa/'>Notre Dame University</a></li>
+            <li><a href='https://webstu.onu.edu/aasu/'>Ohio Northern University</a></li>
+            <li><a href='https://www.facebook.com/aaaohiostate/?fref=ts'>The Ohio State University</a></li>
+            <li><a href='https://campuslink.okstate.edu/organization/asianamerican-student-association'>Oklahoma State University</a></li>
+            <li><a href='https://boilerlink.purdue.edu/organization/purdueaaa'>Purdue University at West Lafayette</a></li>
+            <li><a href='https://www.facebook.com/awa.st.catherine/'>St. Catherine’s University</a></li>
+            <li><a href='https://www.facebook.com/SLUAAA/'>St. Louis University</a></li>
+            <li><a href='https://www.facebook.com/groups/174617512566504/'>University of Colorado Boulder</a></li>
+            <li><a href='https://www.facebook.com/AASIAUIC/'>University of Illinois at Chicago</a></li>
+            <li><a href='https://www.facebook.com/AsianAmericanAssociationUIUC/'>University of Illinois at Urbana Champaign</a></li>
+            <li><a href='https://rockchalkcentral.ku.edu/organization/aasu'>University of Kansas</a></li>
+            <li><a href='https://maizepages.umich.edu/organization/aaa'>University of Michigan – Ann Arbor</a></li>
+            <li><a href='https://bulldoglink.d.umn.edu/organization/apaa'>University of Minnesota – Duluth</a></li>
+            <li><a href='https://www.asu-umn.org/'>University of Minnesota Twin Cities</a></li>
+            <li><a href='https://www.facebook.com/mizzouaaa'>University of Missouri</a></li>
+            <li><a href='https://www.facebook.com/UNLASU'>University of Nebraska Lincoln</a></li>
+            <li><a href='https://www.facebook.com/groups/uwpasiaclub/'>University of Wisconsin – Platteville</a></li>
+            <li><a href='https://win.wisc.edu/organization/aasu'>University of Wisconsin – Madison</a></li>
+            <li><a href='https://www.vandyaasa.com/'>Vanderbilt University</a></li>
+            <li><a href='https://www.valpo.edu/multicultural/student-organizations/asian-american-pacific-island-coalition-aapic/'>Valparaiso University</a></li>
+            <li><a href='https://www.facebook.com/aaa.wustl/'>Washington University – St Louis</a></li>
+            <li><a href='https://www.facebook.com/ASCWSU/'>Wichita State University</a></li>
+          </ul>
         </Page>
         <Page>
+
         For all members schools, they get to select a MAASU Representative. M-Reps, for short,  are the official liaison between member schools and the MAASU Executive Coordinating Committee (ECC). As stated in the MAASU Constitution, each school may only have one M-Rep.
 The roles, duties, and responsibilities of the M-Rep consists of the following:
 Respond to emails and messages from MAASU Communications chairs or other members of the ECC when necessary
