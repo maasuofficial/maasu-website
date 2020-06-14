7cedfffd81a4b3af75710a86de39319871435658
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2020-06-14 12:01:59 -0400

    Convert strings and styles to constants and functional classes

--- src/containers/Contact/index.js
+++ src/containers/Contact/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import { Cell, Grid, Page, Type } from '../../components'
 import querystring from 'querystring'
-import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa'
+import { FaFacebookF, FaInstagram } from 'react-icons/fa'
+import {
+  APP_NAME_LONG,
+  SOCIAL_FACEBOOK,
+  SOCIAL_INSTAGRAM,
+} from 'constants/strings'
 
 class Contact extends React.Component {
   constructor(props) {
@@ -28,7 +33,7 @@ class Contact extends React.Component {
       'entry.1059489069': 'ecc@maasu.org',
       'entry.366340186': name.value,
       'entry.1066764004': email.value,
-      'entry.1137712670': message.value
+      'entry.1137712670': message.value,
     }
 
     const formData = querystring.stringify(response)
@@ -44,7 +49,7 @@ class Contact extends React.Component {
       await fetch(url, options)
       this.setState({
         submission: 'success',
-        submissionMessage: 'Your message was sent.'
+        submissionMessage: 'Your message was sent.',
       })
     } catch (e) {
       this.setState({
@@ -57,41 +62,71 @@ class Contact extends React.Component {
   render() {
     return (
       <Page className={'contact-contact'}>
-        <Type variant='h2'>Contact Us</Type>
+        <Type variant="h2">Contact Us</Type>
 
         <div className={'content-contact'}>
           <div className={'linkContainer-contact'}>
             <Grid className={'linkGrid-contact disp-in-flex'}>
               <Cell auto>
-                <a href='https://facebook.com/maasuofficial'><FaFacebookF /></a>
+                <a href={SOCIAL_FACEBOOK}>
+                  <FaFacebookF />
+                </a>
               </Cell>
+              {/* <Cell auto> */}
+              {/*   <a href="https://twitter.com/MAASUOfficial"> */}
+              {/*     <FaTwitter /> */}
+              {/*   </a> */}
+              {/* </Cell> */}
               <Cell auto>
-                <a href='https://twitter.com/MAASUOfficial'><FaTwitter /></a>
-              </Cell>
-              <Cell auto>
-                <a href='https://www.instagram.com/MAASUOfficial/'><FaInstagram /></a>
+                <a href={SOCIAL_INSTAGRAM}>
+                  <FaInstagram />
+                </a>
               </Cell>
             </Grid>
           </div>
 
-          <p>Any questions? Send us a message and we'll get back to you as soon as possible!</p>
+          <p>
+            Any questions? Send us a message and we'll get back to you as soon
+            as possible!
+          </p>
 
           <form className={'contactForm-contact'} onSubmit={this.handleSubmit}>
-            <input type='text' required name='name' placeholder='First and last name' />
-            <input type='email' required name='email' placeholder='Email' />
-            <textarea required name='message' placeholder='Message'></textarea>
-            <div className={`contactResponse-contact ${this.state.submission ? [`contact-${this.state.submission}-contact`] : ''}`}>
+            <input
+              type="text"
+              required
+              name="name"
+              placeholder="First and last name"
+            />
+            <input type="email" required name="email" placeholder="Email" />
+            <textarea required name="message" placeholder="Message"></textarea>
+            <div
+              className={`contactResponse-contact ${
+                this.state.submission
+                  ? [`contact-${this.state.submission}-contact`]
+                  : ''
+              }`}
+            >
               <span>{this.state.submissionMessage}</span>
             </div>
-            <button type='submit'>Submit</button>
+            <button type="submit">Submit</button>
           </form>
 
           <footer>
-            <span style={{ display: 'none' }}>Special thanks to Craig Bossley for logo design.</span>
             <br />
-            <span>&copy;MAASU 2020. For additional information or questions regarding the website please contact <a className='mail' href='MAILTO:technet@maasu.org'>technet@maasu.org</a>.</span>
+            <span>
+              &copy;MAASU 2020. For additional information or questions
+              regarding the website please contact{' '}
+              <a className="mail" href="MAILTO:technet@maasu.org">
+                technet@maasu.org
+              </a>
+              .
+            </span>
             <br />
-            <img className={'logo-contact'} src={`${process.env.PUBLIC_URL}/assets/branding/light/logo-text.svg`} alt='The Midwest Asian American Students Union' />
+            <img
+              className={'logo-contact'}
+              src={`${process.env.PUBLIC_URL}/assets/branding/light/logo-text.svg`}
+              alt={APP_NAME_LONG}
+            />
           </footer>
         </div>
       </Page>
