94e31caccf4c0bdc9db41bdd388448a55d824570
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2020-08-26 01:10:38 -0400

    Move clickable area to entire hero banner component

--- src/components/HeroBannerComponent.tsx
+++ src/components/HeroBannerComponent.tsx
@@ -10,22 +10,18 @@ import {
 
 export const HeroBannerComponent: FC = () => {
   return (
-    <div className="ttu fw700 py7 mt8 bg-orange1 br">
-      <h2 className="tc ff-hind">
-        <span className="fs3-ns fs2-s dpb">
-          {CONFERENCE_YEAR} {APP_NAME} {CONFERENCE_TYPE}
-        </span>
-        <em className="fsn fs4 fs2-s ls2 ff-libre db">
-          {CONFERENCE_URL ? (
-            <a className="tdn c-gold" href={CONFERENCE_URL}>
-              {CONFERENCE_NAME}
-            </a>
-          ) : (
-            CONFERENCE_NAME
-          )}
-        </em>
-        <span className="fs3-ns fs2-s db">{CONFERENCE_DATE_RANGE}</span>
-      </h2>
-    </div>
+    <a className="c-inh tdn" href={CONFERENCE_URL}>
+      <div className="ttu fw700 py7 mt8 bg-orange1 br">
+        <h2 className="tc ff-hind">
+          <span className="fs3-ns fs2-s dpb">
+            {CONFERENCE_YEAR} {APP_NAME} {CONFERENCE_TYPE}
+          </span>
+          <em className="fsn fs4 fs2-s ls2 ff-libre db">
+            <span className="tdn c-gold">{CONFERENCE_NAME}</span>
+          </em>
+          <span className="fs3-ns fs2-s db">{CONFERENCE_DATE_RANGE}</span>
+        </h2>
+      </div>
+    </a>
   )
 }
