1299959dc58ef8650b484641468b28e382e80ab1
Sam Bossley <bossley.samuel@gmail.com>
2021-01-05 14:40:29 -0500

    Leadership Summit Prep (#46)
    * Update LS url
    
    * Fix node-notifier security vulnerability

--- src/constants/upcoming.ts
+++ src/constants/upcoming.ts
@@ -2,7 +2,7 @@ export const CONFERENCE_NAME = 'Root, Rise, Reflect'
 export const CONFERENCE_TYPE = 'Leadership Summit'
 export const CONFERENCE_YEAR = '2021'
 export const CONFERENCE_DATE_RANGE = 'March 19 - March 22'
-export const CONFERENCE_URL = ''
+export const CONFERENCE_URL = 'https://maasuls2021.org'
 
 export const UPCOMING_ACADEMIC_YEAR = '2021-2022'
 export const SEASON_SPRING = 'SP'
