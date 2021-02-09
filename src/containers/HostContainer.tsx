565a126fff370d407a8abd25be0ced142b4a1c93
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2021-02-08 23:33:50 -0500

    Update ECC, CC, and MS applications

--- src/containers/HostContainer.tsx
+++ src/containers/HostContainer.tsx
@@ -18,38 +18,7 @@ import {
   HOST_PRELUDE_1,
   HOST_1,
   HOST_1_STMT,
-  HOST_1_1,
-  HOST_1_1_A,
-  HOST_1_2,
-  HOST_1_3,
-  HOST_1_3_A,
-  HOST_1_3_B,
-  HOST_1_3_C,
-  HOST_1_4,
-  HOST_1_4_A,
-  HOST_1_5,
-  HOST_1_6,
-  HOST_1_6_A,
-  HOST_1_6_B,
-  HOST_1_6_C,
-  HOST_1_7,
-  HOST_1_7_A,
-  HOST_1_7_B,
-  HOST_1_8,
-  HOST_1_9,
-  HOST_1_9_A,
-  HOST_1_10,
-  HOST_1_11,
-  HOST_1_11_A,
-  HOST_1_11_B,
-  HOST_1_11_C,
-  HOST_1_12,
-  HOST_1_12_A,
-  HOST_1_13,
-  HOST_1_14,
-  HOST_1_14_A,
-  HOST_1_14_A_1,
-  HOST_1_14_A_2,
+  HOST_BID_REQUIREMENTS,
   HOST_2,
   HOST_2_STMT,
   HOST_2_CLOSING,
@@ -103,80 +72,7 @@ export const Host: FC<RouteComponentProps & ReduxProps & Props> = ({
             <li>
               <h5 className="title5">{HOST_1}</h5>
               <p>{HOST_1_STMT}</p>
-              <ol>
-                <li>
-                  <span>{HOST_1_1}</span>
-                  <ol type="a">
-                    <li>{HOST_1_1_A}</li>
-                  </ol>
-                </li>
-                <li>{HOST_1_2}</li>
-                <li>
-                  <span>{HOST_1_3}</span>
-                  <ol type="a">
-                    <li>{HOST_1_3_A}</li>
-                    <li>{HOST_1_3_B}</li>
-                    <li>{HOST_1_3_C}</li>
-                  </ol>
-                </li>
-                <li>
-                  <span>{HOST_1_4}</span>
-                  <ol type="a">
-                    <li>{HOST_1_4_A}</li>
-                  </ol>
-                </li>
-                <li>{HOST_1_5}</li>
-                <li>
-                  <span>{HOST_1_6}</span>
-                  <ol type="a">
-                    <li>{HOST_1_6_A}</li>
-                    <li>{HOST_1_6_B}</li>
-                    <li>{HOST_1_6_C}</li>
-                  </ol>
-                </li>
-                <li>
-                  <span>{HOST_1_7}</span>
-                  <ol type="a">
-                    <li>{HOST_1_7_A}</li>
-                    <li>{HOST_1_7_B}</li>
-                  </ol>
-                </li>
-                <li>{HOST_1_8}</li>
-                <li>
-                  <span>{HOST_1_9}</span>
-                  <ol type="a">
-                    <li>{HOST_1_9_A}</li>
-                  </ol>
-                </li>
-                <li>{HOST_1_10}</li>
-                <li>
-                  <span>{HOST_1_11}</span>
-                  <ol type="a">
-                    <li>{HOST_1_11_A}</li>
-                    <li>{HOST_1_11_B}</li>
-                    <li>{HOST_1_11_C}</li>
-                  </ol>
-                </li>
-                <li>
-                  <span>{HOST_1_12}</span>
-                  <ol type="a">
-                    <li>{HOST_1_12_A}</li>
-                  </ol>
-                </li>
-                <li>{HOST_1_13}</li>
-                <li>
-                  <span>{HOST_1_14}</span>
-                  <ol type="a">
-                    <li>
-                      <span>{HOST_1_14_A}</span>
-                      <ol>
-                        <li>{HOST_1_14_A_1}</li>
-                        <li>{HOST_1_14_A_2}</li>
-                      </ol>
-                    </li>
-                  </ol>
-                </li>
-              </ol>
+              <A href={HOST_BID_REQUIREMENTS}>MAASU Bid Packet Requirements</A>
             </li>
             <p>
               Below are some example bid packets from schools that have hosted
