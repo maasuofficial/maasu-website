81715af614c777f5b83b35295fafe8017d8b93c8
Sam Bossley <bossley.samuel@gmail.com>
2021-04-17 04:17:08 +0000

    BOA typo (#55)

--- src/constants/pages.ts
+++ src/constants/pages.ts
@@ -152,7 +152,7 @@ export const DC_APP_STATUS = `Directors Council ${dcAppStatus.status}`
 
 export const BOA_APP_ISOPEN = true
 export const BOA_APP_STATUS = `
-We currently have have an opening on the Board of Advisors and are looking for qualified candidates who reside in the Midwest with a professional background and expertise in finance, fundraising, accounting or a licensed CPA.
+We currently have an opening on the Board of Advisors and are looking for qualified candidates who reside in the Midwest with a professional background and expertise in finance, fundraising, accounting or a licensed CPA.
 Deadline is on a rolling basis.
 `
 
