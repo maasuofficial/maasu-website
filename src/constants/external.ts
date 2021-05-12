526954f4fce1199d728ec4d8b280ee519c4ecdf8
Sam Bossley <bossley.samuel@gmail.com>
2021-05-12 15:56:09 -0400

    DC Application (#60)
    * Open DC app and close ECC app
    
    * Update to fix vulnerabilities and switch to js sass

--- src/constants/external.ts
+++ src/constants/external.ts
@@ -2,25 +2,21 @@
 // Home
 // -----------------------------------------------------
 
-export const HERO_BANNER_IMG = `
-https://firebasestorage.googleapis.com/v0/b/maasu-master.appspot.com/o/Pages%2FHome%2FheroBanner2.png?alt=media&token=d11fb21f-320b-485e-9604-ea9ac192951d
-`
+export const HERO_BANNER_IMG =
+  'https://firebasestorage.googleapis.com/v0/b/maasu-master.appspot.com/o/Pages%2FHome%2FheroBanner2.png?alt=media&token=d11fb21f-320b-485e-9604-ea9ac192951d'
 
 // -----------------------------------------------------
 // Mission
 // -----------------------------------------------------
 
-export const STRATEGIC_PLAN_IMG = `
-https://firebasestorage.googleapis.com/v0/b/maasu-master.appspot.com/o/Pages%2FMission%2FstrategicPlan.jpg?alt=media&token=caf1a130-bf56-4c56-bc34-13f048b2ff30
-`
+export const STRATEGIC_PLAN_IMG =
+  'https://firebasestorage.googleapis.com/v0/b/maasu-master.appspot.com/o/Pages%2FMission%2FstrategicPlan.jpg?alt=media&token=caf1a130-bf56-4c56-bc34-13f048b2ff30'
 
-export const STRATEGIC_PLAN_URL = `
-https://firebasestorage.googleapis.com/v0/b/maasu-master.appspot.com/o/Plans%2F20-24.pdf?alt=media&token=6e86c645-0c70-4cb1-8b1f-18a95edc6118
-`
+export const STRATEGIC_PLAN_URL =
+  'https://firebasestorage.googleapis.com/v0/b/maasu-master.appspot.com/o/Plans%2F20-24.pdf?alt=media&token=6e86c645-0c70-4cb1-8b1f-18a95edc6118'
 
-export const MISSION_FIRST_MEETING = `
-https://firebasestorage.googleapis.com/v0/b/maasu-master.appspot.com/o/Pages%2FMission%2FfirstMeeting.jpg?alt=media&token=ef300262-d49b-41d9-a608-8eb473623585
-`
+export const MISSION_FIRST_MEETING =
+  'https://firebasestorage.googleapis.com/v0/b/maasu-master.appspot.com/o/Pages%2FMission%2FfirstMeeting.jpg?alt=media&token=ef300262-d49b-41d9-a608-8eb473623585'
 
 // -----------------------------------------------------
 // Board
@@ -28,14 +24,13 @@ https://firebasestorage.googleapis.com/v0/b/maasu-master.appspot.com/o/Pages%2FM
 
 export const ED_APP_LINK =
   'https://firebasestorage.googleapis.com/v0/b/maasu-master.appspot.com/o/BoardMembers%2F20-21%2Fapp-ed.pdf?alt=media&token=7ae31959-8f0b-42f8-8a28-69e836288fb4'
-export const DC_APP_LINK = `${process.env.PUBLIC_URL}/assets/files/MAASU-ED-Application-2020-2022-new.docx`
-export const BOA_APP_LINK = `
-https://firebasestorage.googleapis.com/v0/b/maasu-master.appspot.com/o/BoardMembers%2F20-21%2Fapp-boa.pdf?alt=media&token=a2d3751c-cab6-4bb3-8e73-0dbae5c0c782
-`
+export const DC_APP_LINK =
+  'https://firebasestorage.googleapis.com/v0/b/maasu-master.appspot.com/o/BoardMembers%2F20-21%2Fapp-dc.pdf?alt=media&token=40971f87-73fb-41c1-b2e4-5c5763f73d0f'
+export const BOA_APP_LINK =
+  'https://firebasestorage.googleapis.com/v0/b/maasu-master.appspot.com/o/BoardMembers%2F20-21%2Fapp-boa.pdf?alt=media&token=a2d3751c-cab6-4bb3-8e73-0dbae5c0c782'
 
-export const ECC_APP_LINK = `
-https://docs.google.com/forms/d/e/1FAIpQLSf6xqAaFKkqAKEzzAmP9QkYaSQ2dR0OUx8-h8TsndVjYBVtaw/viewform
-`
+export const ECC_APP_LINK =
+  'https://docs.google.com/forms/d/e/1FAIpQLSf6xqAaFKkqAKEzzAmP9QkYaSQ2dR0OUx8-h8TsndVjYBVtaw/viewform'
 
 // -----------------------------------------------------
 // Donate
@@ -46,74 +41,60 @@ export const DONATE_MAASU_URL =
 export const DONATE_MAA_URL =
   'https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=LFY3B4ZT98TVN&source=url'
 
-export const DONATE_HERO_IMG = `
-https://firebasestorage.googleapis.com/v0/b/maasu-master.appspot.com/o/Pages%2FDonate%2FdonateHero.jpg?alt=media&token=12fa83b1-0385-47cf-ad34-f7e967389b52
-`
+export const DONATE_HERO_IMG =
+  'https://firebasestorage.googleapis.com/v0/b/maasu-master.appspot.com/o/Pages%2FDonate%2FdonateHero.jpg?alt=media&token=12fa83b1-0385-47cf-ad34-f7e967389b52'
 
 // -----------------------------------------------------
 // Hosting a Conference
 // -----------------------------------------------------
 
-export const HOST_FORM = `
-https://docs.google.com/forms/d/e/1FAIpQLSfv5mgoV5_e327bdfgA3JAdW5n8LxU0E0jw1N2Bfb4l-QSo9g/viewform
-`
+export const HOST_FORM =
+  'https://docs.google.com/forms/d/e/1FAIpQLSfv5mgoV5_e327bdfgA3JAdW5n8LxU0E0jw1N2Bfb4l-QSo9g/viewform'
 
-export const HOST_BID_REQUIREMENTS = `
-https://firebasestorage.googleapis.com/v0/b/maasu-master.appspot.com/o/Pages%2FHost%2FbidRequirements.pdf?alt=media&token=58dd172b-a132-4e2c-9705-5767acf358df
-`
+export const HOST_BID_REQUIREMENTS =
+  'https://firebasestorage.googleapis.com/v0/b/maasu-master.appspot.com/o/Pages%2FHost%2FbidRequirements.pdf?alt=media&token=58dd172b-a132-4e2c-9705-5767acf358df'
 
 // -----------------------------------------------------
 // Directors Council
 // -----------------------------------------------------
 
-export const DC_PROJECT_LIST = `
-https://bit.ly/3aZ9u87
-`
+export const DC_PROJECT_LIST = 'https://bit.ly/3aZ9u87'
 
-export const DC_PROJECT_APP_FORM = `
-https://docs.google.com/forms/d/e/1FAIpQLSfUsKp2kqs64fW6reA13IuN1ppAbmssytsKeDhyhqMB1MmIgw/viewform
-`
+export const DC_PROJECT_APP_FORM =
+  'https://docs.google.com/forms/d/e/1FAIpQLSfUsKp2kqs64fW6reA13IuN1ppAbmssytsKeDhyhqMB1MmIgw/viewform'
 
 // -----------------------------------------------------
 // Alumni
 // -----------------------------------------------------
 
-export const MAA_LOGO_IMG = `
-https://firebasestorage.googleapis.com/v0/b/maasu-master.appspot.com/o/Pages%2FAlumni%2Fmaa.png?alt=media&token=4a95987b-0377-46d6-8740-12d8f58aee8b
-`
+export const MAA_LOGO_IMG =
+  'https://firebasestorage.googleapis.com/v0/b/maasu-master.appspot.com/o/Pages%2FAlumni%2Fmaa.png?alt=media&token=4a95987b-0377-46d6-8740-12d8f58aee8b'
 
-export const MAA_ALUMNI_DIR_FORM = `
-https://docs.google.com/forms/d/e/1FAIpQLScepfU3sMqFNlCWTBBnV9ig2vTk6Z_E9Ar9MxmK8DnHKU30Gw/viewform?c=0&w=1
-`
+export const MAA_ALUMNI_DIR_FORM =
+  'https://docs.google.com/forms/d/e/1FAIpQLScepfU3sMqFNlCWTBBnV9ig2vTk6Z_E9Ar9MxmK8DnHKU30Gw/viewform?c=0&w=1'
 
-export const MAA_MEMBERSHIP_FEE_URL = `
-https://www.paypal.com/cgi-bin/webscr&cmd=_s-xclick&hosted_button_id=NXTRNY49H77YQ
-`
+export const MAA_MEMBERSHIP_FEE_URL =
+  'https://www.paypal.com/cgi-bin/webscr&cmd=_s-xclick&hosted_button_id=NXTRNY49H77YQ'
 
 // -----------------------------------------------------
 // Awards
 // -----------------------------------------------------
 
-export const AWARDS_CC_LINK = `
-https://firebasestorage.googleapis.com/v0/b/maasu-master.appspot.com/o/Pages%2FAwards%2FccAward.pdf?alt=media&token=748f43dd-e44b-47ff-8dc0-7d6ab85126f7
-`
+export const AWARDS_CC_LINK =
+  'https://firebasestorage.googleapis.com/v0/b/maasu-master.appspot.com/o/Pages%2FAwards%2FccAward.pdf?alt=media&token=748f43dd-e44b-47ff-8dc0-7d6ab85126f7'
 
-export const AWARDS_MS_LINK = `
-https://firebasestorage.googleapis.com/v0/b/maasu-master.appspot.com/o/Pages%2FAwards%2FmsAward.pdf?alt=media&token=bb7c18b9-da25-458d-bc46-1de0a23e6c80
-`
+export const AWARDS_MS_LINK =
+  'https://firebasestorage.googleapis.com/v0/b/maasu-master.appspot.com/o/Pages%2FAwards%2FmsAward.pdf?alt=media&token=bb7c18b9-da25-458d-bc46-1de0a23e6c80'
 
 // -----------------------------------------------------
 // Annual
 // -----------------------------------------------------
 
-export const ANNUAL_REPORT_IMG = `
-https://firebasestorage.googleapis.com/v0/b/maasu-master.appspot.com/o/Pages%2FAnnual%2FannualReport.jpg?alt=media&token=56006814-21b4-4497-ad94-be2a35008dbe
-`
+export const ANNUAL_REPORT_IMG =
+  'https://firebasestorage.googleapis.com/v0/b/maasu-master.appspot.com/o/Pages%2FAnnual%2FannualReport.jpg?alt=media&token=56006814-21b4-4497-ad94-be2a35008dbe'
 
 // -----------------------------------------------------
 // Post Requests
 // -----------------------------------------------------
 
-export const POST_REQUEST_FORM_URL = `
-https://forms.gle/LQsRC8UkjRemLuqj9
-`
+export const POST_REQUEST_FORM_URL = 'https://forms.gle/LQsRC8UkjRemLuqj9'
