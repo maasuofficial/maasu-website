91c36b4ab06ef7b1f4a0c913faf73df57126f032
Sam Bossley <sam@bossley.xyz>
2024-04-06 00:02:55 -0400

    Revert "feat: close awards"
    This reverts commit 1eba3d790c6b7b59a65eda10e151e95665fd6b90.

--- src/containers/AwardsContainer.tsx
+++ src/containers/AwardsContainer.tsx
@@ -1,5 +1,6 @@
 import { RouteComponentProps } from '@reach/router'
 import { useDocumentTitle } from '../hooks/meta'
+import { A } from '../components/Link'
 import { Block } from '../components/Block'
 import { Container } from '../components/Container'
 import { TextBlock } from '../components/TextBlock'
@@ -65,6 +66,11 @@ export function AwardsContainer(_: RouteComponentProps) {
               dedication to the APIDA community, along with a $250 scholarship
               and plaque.
             </p>
+            <p>
+              Applications are open! The submission deadline is April 3nd, 2024
+              at 11:59 PM MT (12:59 AM CT, 1:59 AM ET).{' '}
+              <A href="https://forms.gle/vmr3uLqQdqxGmMNw6">Apply here.</A>
+            </p>
             {renderAwardsTable(
               'Charles Chang Leadership',
               charlesChangAwardees
@@ -80,6 +86,11 @@ export function AwardsContainer(_: RouteComponentProps) {
               means that have uplifted a community higher than what has been
               done previously.
             </p>
+            <p>
+              Applications are open! The submission deadline is April 3nd, 2024
+              at 11:59 PM MT (12:59 AM CT, 1:59 AM ET).{' '}
+              <A href="https://forms.gle/MLADDtj5iu6R1AQa7">Apply here.</A>
+            </p>
             {renderAwardsTable('Midwestern Star', midwesternStarAwardees)}
           </li>
 
@@ -91,6 +102,11 @@ export function AwardsContainer(_: RouteComponentProps) {
               their campus and in the Midwest community. The BOA Award and $100
               prize is presented annually at the MAASU Spring Conference banquet
               to one organization or school.
+            </p>
+            <p>
+              Applications are open! The submission deadline is April 3nd, 2024
+              at 11:59 PM MT (12:59 AM CT, 1:59 AM ET).{' '}
+              <A href="https://forms.gle/TeQkYWA3DFma9EU56">Apply here.</A>
             </p>
             {renderAwardsTable('Board of Advisors (BOA)', boaAwardees)}
           </li>
